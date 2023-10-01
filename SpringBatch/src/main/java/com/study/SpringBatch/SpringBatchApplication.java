7635115367cbc9b1cec7a116af4d67cb2f92a0ae
HyoungKi <ngsk09@naver.com>
2023-10-01 23:45:18 +0900

    SpringBatch 초기 설정 및 HelloWorld 찍어보기

---
+++ SpringBatch/src/main/java/com/study/SpringBatch/SpringBatchApplication.java
@@ -0,0 +1,14 @@
+package com.study.SpringBatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableBatchProcessing //배치기능 활성화
+@SpringBootApplication
+public class SpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchApplication.class, args);
+	}
+
+}
